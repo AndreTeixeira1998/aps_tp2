1b97de5cc2639bf54c27daceade5ead4dad1e06c
ruimscarvalho98 <ruimsc98@gmail.com>
2020-04-26 14:29:00 +0100

    custom funciona

---
+++ build/CMakeFiles/aps_tp2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/aps_tp2.dir/main.c.o"
+  "aps_tp2"
+  "aps_tp2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/aps_tp2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
